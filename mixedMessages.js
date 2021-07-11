3fb68106d5ec7553a93f747596576c46236ff4da
dreaKeown <andrea.keown13@gmail.com>
2021-07-11 09:12:53 +0100

    Random number, color and messages functions complete

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,6 +1,26 @@
-function getRandomNumber() {
-    let number = Math.floor(Math.random() * 10);
-    return number;
+function randomNumber() {
+    return number = Math.floor(Math.random() * 10);
+    
 }
 
-console.log(getRandomNumber());
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+function randomColor(colors) {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+
+const messages = ["A life spent",
+                  "If you don't",
+                  "Challenges are what",
+                  "A mind that is stretched",
+                  "Most people do not listen",
+                  "If you don't",
+                  "The most common",
+                  "A person who never made a mistake"];
+
+function randomMessage(messages) {
+    return messages[Math.floor(Math.random() * messages.length)];
+}
+
+console.log(`Your lucky number is: ${randomNumber()}, your lucky color is: ${randomColor(colors)} and your inspirational message is: ${randomMessage(messages)} `); //Test
