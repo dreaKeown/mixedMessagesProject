339fed41ecf6cb7a4093cf22dd6717600dc28d71
dreaKeown <86792982+dreaKeown@users.noreply.github.com>
2021-07-13 11:47:40 +0100

    Update mixedMessages.js
    Fixed missing / mistypes quote marks on two of the messages.

--- mixedMessages.js
+++ mixedMessages.js
@@ -14,7 +14,7 @@ const messages = ["'A life spent making mistakes is not only more honorable but 
                   "'If you don't like something, change it. If you can't change it, change the way you think about it.' - Mary Engelbreit",
                   "'Challenges are what make life interesting and overcoming them is what makes life meaningful.' - Joshua J Marine",
                   "'A mind that is stretched by a new experience can never go back to it's old dimensions.' - Oliver Wendell Holmes",
-                  "Most people do not listen with the intent to understand, they listen with the intent to reply. - Stephen Covey",
+                  "'Most people do not listen with the intent to understand, they listen with the intent to reply.' - Stephen Covey",
                   "'If you don't like the road you're walking, start paving another one.' - Dolly Parton",
                   "'The most common way people give up their power is by thinking they don't have any.' - Alice Walker",
                   "'A person who never made a mistake never tried anything new. - Albert Einstein'"];
